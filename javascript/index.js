ca92146ce44b80fb4bd07296fc1bf4c7ef399235
Muhammad Mudassir <mudassirkhn940@gmail.com>
2024-05-25 17:50:49 +0530

    commit new file

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+ScrollReveal({ 
+    reset: true,
+    distance :"60px",
+    duration :2500,
+    delay :400 
+ });
+
+    ScrollReveal().reveal('.banner1 h1, .banner2_sec  ,.banner3 img, .banner4_sec, .banner5 img,.banner6_sec,.banner7_heading h2, .banner7 .img', { opacity:0, scale:0.8 ,interval: 600   });
+   ScrollReveal().reveal('.banner2 img , .banner3_sec, .banner4 img ,.banner5_sec,.banner6 img, .banner7_sec', { opacity:0, scale:0.8 ,interval: 600   });
